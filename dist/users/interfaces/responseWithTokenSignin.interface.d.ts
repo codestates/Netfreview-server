9bdd386626ef28763c53a0846d18af283b0bef89
최재선 <js@choejaeseon-ui-MacBookPro.local>
2021-03-14 22:36:14 +0900

    update videolist aboutThis

---
+++ dist/users/interfaces/responseWithTokenSignin.interface.d.ts
@@ -0,0 +1,8 @@
+import { User } from 'src/entity/User.entity';
+export interface ResponseWithTokenSignin {
+    data: {
+        accessToken: string;
+    };
+    user: User;
+    message: string;
+}
